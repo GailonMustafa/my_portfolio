92275a1917264926bce38965249289ff2caf14aa
Gailon Mustafa <gailonmustafa2@gmail.com>
2025-05-22 08:10:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+console.log("Website loaded!");
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("form-status").textContent = "Thanks for your message!";
+  this.reset();
+});
+
+console.log("Website loaded!");
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("form-status").textContent = "Thanks for your message!";
+  this.reset();
+});
