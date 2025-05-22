1f1ac419be8200b37f42a16a26e605424a77473b
Gailon Mustafa <gailonmustafa2@gmail.com>
2025-05-21 20:56:43 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Website loaded!");
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("form-status").textContent = "Thanks for your message!";
+  this.reset();
+});
